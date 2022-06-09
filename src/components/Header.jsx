822016ff0054e957922b798f455ed3d52c1fc9fd
isaias-alt <lucasisaiascasco@gmail.com>
2022-06-09 02:41:26 -0400

    Add a navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import { useState } from "react";
+import { AppBar, makeStyles, Toolbar } from "@material-ui/core";
 
 const Header = () => {
-  return (
-    <div>
-      <h1>Header</h1>
-    </div>
-  );
+  const [mobile, setMobile] = useState(false);
+  const classes = useStyle();
+
+  const displayMobile = () => {};
+  //TODO: add a logo and images
+  const displayDesktop = () => {
+    return (
+      <Toolbar className={classes.toolbar}>
+        <h1 className={classes.logo}>estoy dentro del toolbar</h1>
+      </Toolbar>
+    );
+  };
+
+  return <AppBar>{mobile ? displayMobile() : displayDesktop()}</AppBar>;
 };
+
+const useStyle = makeStyles((theme) => ({
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  logo: {
+    height: "50px",
+    margin: theme.spacing(1, 0, 0, 2),
+    objectFit: "contain",
+  },
+}));
 
 export default Header;
