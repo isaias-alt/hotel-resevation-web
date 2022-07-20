8ebbb742b08fe740f9b5b08a97ebb7e1618e9350
isaias-alt <lucasisaiascasco@gmail.com>
2022-07-20 16:45:41 -0400

    adding date range picker

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -1,11 +1,36 @@
 import React from "react";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { DateRangePicker } from "react-date-range";
+import { makeStyles } from "@material-ui/core";
+
+const selectionRange = {
+  startDate: new Date(20, 7, 2022),
+  endDate: new Date(20, 7, 2022),
+  key: "selection",
+};
 
 const DatePicker = () => {
+  const classes = useStyle();
+  const handleSelect = () => {};
+
   return (
-    <div>
-      <h1>Date Picker</h1>
+    <div className={classes.root}>
+      <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
     </div>
   );
 };
+
+const useStyle = makeStyles((theme) => ({
+  root: {
+    position: "absolute",
+    top: "12vh",
+    left: "30vw",
+    [theme.breakpoints.down("xs")]: {
+      top: "12vh",
+      left: 0,
+    },
+  },
+}));
 
 export default DatePicker;
